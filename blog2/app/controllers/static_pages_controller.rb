6e67ce3e6cca544996abd3be7d2b2eff583b2890
Chris Bagi <chris.bagi@gmail.com>
2015-12-29 08:47:17 -0500

    author message on landing page

---
+++ blog2/app/controllers/static_pages_controller.rb
@@ -0,0 +1,29 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def contact
+  end
+
+  def about
+  end
+
+  def disclaimer
+  end
+
+  def videos
+  end
+
+  def interviews
+  end
+
+  def podcasts
+  end
+
+  def donate
+  end
+
+  def speaking
+  end
+
+end
